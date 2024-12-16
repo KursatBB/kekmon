95b3ec7ffe50dc2a324e947060f53608c22ff63c
Kürşat <66494923+KursatBB@users.noreply.github.com>
2024-12-16 10:50:15 +0300

    Update kekmon.sh
    İnternet arayüzü ve yenileme süresi kullanıcıdan alınıyor.

--- kekmon.sh
+++ kekmon.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-# İzlemek istediğiniz ağ arayüzü
-interface="eth0"
+function usage {
+    echo "Kullanım: $0 <ağ_arayüzü> <yenileme_süresi>"
+    echo "Örnek: $0 ens160 1"
+    exit 1
+}
 
-# Yenileme süresi (saniye)
-interval=1
+if [[ $# -ne 2 ]]; then
+    echo "Hata: Eksik argümanlar."
+    usage
+fi
+
+interface=$1
+interval=$2
+
+if [[ ! -d /sys/class/net/$interface ]]; then
+    echo "Hata: Belirtilen ağ arayüzü ($interface) mevcut değil."
+    exit 1
+fi
+
+if ! [[ $interval =~ ^[0-9]+$ ]]; then
+    echo "Hata: Yenileme süresi ($interval) geçerli bir pozitif sayı olmalıdır."
+    exit 1
+fi
+
 sec=1
-
-# Toplamlar için değişkenler
 total_in_kbps=0
 total_out_kbps=0
 total_in_mbps=0
@@ -15,24 +32,26 @@ total_out_mbps=0
 total_in_mbits=0
 total_out_mbits=0
 
-# Başlık
 printf "%-4s %-12s %-12s %-12s %-12s %-12s %-12s\n" "Sec" "KB/s In" "KB/s Out" "MB/s In" "MB/s Out" "Mbit/s In" "Mbit/s Out"
 
-# Eski veri sayacı başlangıcı
 old_in=$(cat /sys/class/net/$interface/statistics/rx_bytes)
 old_out=$(cat /sys/class/net/$interface/statistics/tx_bytes)
 
-# Çıkışta ortalamayı hesaplayan fonksiyon
 function calculate_average {
     echo
     echo "Araç sonlandırıldı. Ortalama değerler hesaplanıyor..."
 
-    avg_in_kbps=$(awk "BEGIN {printf \"%.3f\", $total_in_kbps / $sec}")
-    avg_out_kbps=$(awk "BEGIN {printf \"%.3f\", $total_out_kbps / $sec}")
-    avg_in_mbps=$(awk "BEGIN {printf \"%.3f\", $total_in_mbps / $sec}")
-    avg_out_mbps=$(awk "BEGIN {printf \"%.3f\", $total_out_mbps / $sec}")
-    avg_in_mbits=$(awk "BEGIN {printf \"%.3f\", $total_in_mbits / $sec}")
-    avg_out_mbits=$(awk "BEGIN {printf \"%.3f\", $total_out_mbits / $sec}")
+    if [[ $sec -eq 1 ]]; then
+        echo "Yeterli veri yok, ölçümler yapılmadı."
+        exit 0
+    fi
+
+    avg_in_kbps=$(awk "BEGIN {printf \"%.3f\", $total_in_kbps / ($sec - 1)}")
+    avg_out_kbps=$(awk "BEGIN {printf \"%.3f\", $total_out_kbps / ($sec - 1)}")
+    avg_in_mbps=$(awk "BEGIN {printf \"%.3f\", $total_in_mbps / ($sec - 1)}")
+    avg_out_mbps=$(awk "BEGIN {printf \"%.3f\", $total_out_mbps / ($sec - 1)}")
+    avg_in_mbits=$(awk "BEGIN {printf \"%.3f\", $total_in_mbits / ($sec - 1)}")
+    avg_out_mbits=$(awk "BEGIN {printf \"%.3f\", $total_out_mbits / ($sec - 1)}")
 
     echo
     printf "Ortalama KB/s In: %s\n" "$avg_in_kbps"
@@ -43,22 +62,23 @@ function calculate_average {
     printf "Ortalama Mbit/s Out: %s\n" "$avg_out_mbits"
 }
 
-# Script sonlandırıldığında ortalama hesaplama
 trap calculate_average EXIT
 
 while true
 do
     sleep $interval
 
-    # Yeni veri sayacı
     new_in=$(cat /sys/class/net/$interface/statistics/rx_bytes)
     new_out=$(cat /sys/class/net/$interface/statistics/tx_bytes)
 
-    # Fark hesaplama
     in_diff=$((new_in - old_in))
     out_diff=$((new_out - old_out))
 
-    # Baytları KB, MB ve Mbit'e çevirme ve virgül ile ayırma (ilk 3 ondalık basamak)
+    if [[ $in_diff -lt 0 || $out_diff -lt 0 ]]; then
+        echo "Hata: Negatif fark tespit edildi. Veriler sıfırlanmış olabilir."
+        exit 1
+    fi
+
     in_kbps=$(awk "BEGIN {printf \"%.3f\", $in_diff / 1024}")
     out_kbps=$(awk "BEGIN {printf \"%.3f\", $out_diff / 1024}")
 
@@ -68,10 +88,8 @@ do
     in_mbits=$(awk "BEGIN {printf \"%.3f\", $in_diff * 8 / 1024 / 1024}")
     out_mbits=$(awk "BEGIN {printf \"%.3f\", $out_diff * 8 / 1024 / 1024}")
 
-    # Çıktı (printf ile hizalı)
     printf "%-4d %-12s %-12s %-12s %-12s %-12s %-12s\n" "$sec" "$in_kbps" "$out_kbps" "$in_mbps" "$out_mbps" "$in_mbits" "$out_mbits"
-    
-    # Toplamları hesapla
+
     total_in_kbps=$(awk "BEGIN {print $total_in_kbps + $in_kbps}")
     total_out_kbps=$(awk "BEGIN {print $total_out_kbps + $out_kbps}")
     total_in_mbps=$(awk "BEGIN {print $total_in_mbps + $in_mbps}")
@@ -81,7 +99,6 @@ do
 
     sec=$((sec + 1))
 
-    # Eski sayacı güncelle
     old_in=$new_in
     old_out=$new_out
 done
